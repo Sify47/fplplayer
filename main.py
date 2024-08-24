6a6f83de96f4fe89beef17f07be7b83d92785548
SIFY <154904386+Sify47@users.noreply.github.com>
2024-08-24 18:46:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,44 +12,12 @@ def main(page : Page):
     page.vertical_alignment = "center"
     page.scroll = "auto"
     
-    def p():
-            txt_name = ft.TextField(label="Your ID")
-
-            page.add(txt_name, ft.ElevatedButton("Show Team"))
-    def _top():
-        def btn_click(e):
-            TextField(label="Your IDewfs")
-        top = Container(
-            gradient=LinearGradient(
-                                begin=alignment.bottom_left,
-                                end=alignment.top_right,
-                                colors=['lightblue660' , 'lightblue900']
-                            ),
-            border_radius=35,
-            content=Column(
-                alignment='center',
-                controls=[
-                    Row(
-                        alignment='center',
-                        controls=[
-                            TextField(label="Your ID")
-                        ]
-                    )
-                ]
-            )
+    page.add(
+        Row(
+            [
+                Text("HeLLLLLLLLLO")
+            ]
         )
-        return top
-    
-    _c = Container(
-                    width=310,
-                    height=660,
-                    border_radius=35,
-                    bgcolor='black',
-                    padding=10,
-                    content=Stack(
-                        width=300,controls=[_top()]
-                    )
     )
-    page.add(_c)
 if __name__ == "__main__":
     ft.app(target=main)
